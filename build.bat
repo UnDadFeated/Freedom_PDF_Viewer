4ef9eb514efab2a860a5155ceb280971daa9757f
UnDadFeated <undadfeated@users.noreply.github.com>
2026-03-11 16:44:19 -0700

    Update build.bat to v3.1.5

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
-echo Building Freedom PDF Viewer v3.1.4 for release...
+echo Building Freedom PDF Viewer v3.1.5 for release...
 
 :: Define output filename
-set OUTPUT_ZIP=Freedom_PDF_Viewer_v3.1.4.zip
+set OUTPUT_ZIP=Freedom_PDF_Viewer_v3.1.5.zip
 
 :: Remove old zip if it exists
 if exist "%OUTPUT_ZIP%" del "%OUTPUT_ZIP%"
